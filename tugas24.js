e74a8f56baf12f8cbfbe8b152c8b7671b577ed98
Firman Ismail <akayasha99@gmail.com>
2022-02-11 21:31:57 +0700

    tugas 24 js

---
+++ tugas24.js
@@ -0,0 +1,7 @@
+var arr =[32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66 ];
+asscending = arr.sort();
+descending = arr.reverse();
+
+console.log("Sebelumnya : ",arr);
+console.log("Asscendig : ",asscending);
+console.log("Descending : ",descending);
